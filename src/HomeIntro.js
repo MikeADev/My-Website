180eabbf6fc933b71cfa0cdb301bff13ce8690cb
MikeADev <mikearcherdev@gmail.com>
2020-03-27 12:00:28 -0500

    initial setup, created nav

---
+++ src/HomeIntro.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function HomeIntro(props) {
+    return (
+        <div className="homeIntro">
+            <div className="htmltag">
+                <p>&lt;h1&gt;</p>
+            </div>
+            <h1>Hi,</h1>
+            <h1>I'm Mike,</h1>
+            <h1>web developer.</h1>
+            <div className="htmltag">
+                <p>&lt;h1&gt;</p>
+            </div>
+            <h6>Full Stack Developer / Freelancer</h6>
+            <div className="contactBtn"><a href="contact">Contact</a></div>
+        </div>
+    )
+}
+
+export default HomeIntro
---
+++ src/SideNav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SideNav extends React.Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li><a className="active" href="home"><i className="fa fa-home fa-3x"></i><b>home</b></a></li>
+                    <li><a href="about"><i className="fa fa-user fa-3x"></i><b>about</b></a></li>
+                    <li><a href="skills"><i className="fa fa-cog fa-3x"></i><b>skills</b></a></li>
+                    <li><a href="portfolio"><i className="fa fa-eye fa-3x"></i><b>my work</b></a></li>
+                    <li><a href="contact"><i className="fa fa-envelope fa-3x"></i><b>contact</b></a></li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default SideNav;
